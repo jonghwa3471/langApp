2d651f7c8d768a76fe281cc9650e4bdea34782c3
Jonghwa <whdghk3471@gmail.com>
2026-02-13 18:22:07 +0900

    #4.18 Drag and Drop Project part Two

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 import icons from "./icons";
 import { useRef } from "react";
-import { Animated, PanResponder, View } from "react-native";
+import { Animated, Dimensions, Easing, PanResponder, View } from "react-native";
 
 const BLACK_COLOR = "#1e272e";
 const GRAY = "#485460";
@@ -39,6 +39,7 @@ const Center = styled.View`
   flex: 3;
   justify-content: center;
   align-items: center;
+  z-index: 1;
 `;
 
 const IconCard = styled(Animated.createAnimatedComponent(View))`
@@ -47,7 +48,17 @@ const IconCard = styled(Animated.createAnimatedComponent(View))`
   border-radius: 10px;
 `;
 
+const { height: WINDOW_HEIGHT } = Dimensions.get("window");
+const topDimensionMax = -WINDOW_HEIGHT / 2;
+const topDimensionMin = -WINDOW_HEIGHT / 20;
+const bottomDimensionMax = WINDOW_HEIGHT / 2;
+const bottomDimensionMin = WINDOW_HEIGHT / 20;
+
+const topLimit = -WINDOW_HEIGHT / 3;
+const bottomLimit = WINDOW_HEIGHT / 3;
+
 export default function App() {
+  const opacity = useRef(new Animated.Value(1)).current;
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(
     new Animated.ValueXY({
@@ -55,6 +66,16 @@ export default function App() {
       y: 0,
     }),
   ).current;
+  const scaleOne = position.y.interpolate({
+    inputRange: [topDimensionMax, topDimensionMin],
+    outputRange: [2, 1],
+    extrapolate: "clamp",
+  });
+  const scaleTow = position.y.interpolate({
+    inputRange: [bottomDimensionMin, bottomDimensionMax],
+    outputRange: [1, 2],
+    extrapolate: "clamp",
+  });
 
   const onPressIn = Animated.spring(scale, {
     toValue: 0.9,
@@ -74,23 +95,55 @@ export default function App() {
     useNativeDriver: true,
   });
 
+  const onDropScale = Animated.timing(scale, {
+    toValue: 0,
+    useNativeDriver: true,
+    duration: 200,
+    easing: Easing.linear,
+  });
+
+  const onDropOpacity = Animated.timing(opacity, {
+    toValue: 0,
+    duration: 200,
+    useNativeDriver: true,
+    easing: Easing.linear,
+  });
+
+  const onDropPosition = Animated.timing(position, {
+    toValue: {
+      x: 0,
+      y: 0,
+    },
+    useNativeDriver: true,
+    duration: 200,
+    easing: Easing.linear,
+  });
+
   const panResponder = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderGrant: () => {
         onPressIn.start();
       },
-      onPanResponderMove: (_, { dx, dy }) =>
-        position.setValue({ x: dx, y: dy }),
-      onPanResponderRelease: () => {
-        Animated.parallel([onPressOut, goHome]).start();
+      onPanResponderMove: (_, { dx, dy }) => {
+        position.setValue({ x: dx, y: dy });
+      },
+      onPanResponderRelease: (_, { dy }) => {
+        if (dy < topLimit || dy > bottomLimit) {
+          Animated.sequence([
+            Animated.parallel([onDropOpacity, onDropScale]),
+            onDropPosition,
+          ]).start();
+        } else {
+          Animated.parallel([onPressOut, goHome]).start();
+        }
       },
     }),
   ).current;
   return (
     <Container>
       <Edge>
-        <WordContainer>
+        <WordContainer style={{ transform: [{ scale: scaleOne }] }}>
           <Word $color={GREEN}>알아</Word>
         </WordContainer>
       </Edge>
@@ -98,14 +151,15 @@ export default function App() {
         <IconCard
           {...panResponder.panHandlers}
           style={{
-            transform: [{ scale }, ...position.getTranslateTransform()],
+            opacity,
+            transform: [...position.getTranslateTransform(), { scale }],
           }}
         >
           <Ionicons name="beer" color={GRAY} size={76} />
         </IconCard>
       </Center>
       <Edge>
-        <WordContainer>
+        <WordContainer style={{ transform: [{ scale: scaleTow }] }}>
           <Word $color={RED}>몰라</Word>
         </WordContainer>
       </Edge>
